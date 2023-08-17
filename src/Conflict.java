2a11ce9a11184675d624d9c1679615142fb54c5c
mamatha.m <mmamatha0808@gmail.com>
2023-08-17 13:16:53 +0530

    some text added

--- src/Conflict.java
+++ src/Conflict.java
@@ -3,5 +3,11 @@ public class Conflict
     public static void main(String[] args){
         System.out.println("hii,welcome");
         System.out.println("hii,hello");
+        add();
+
+    }
+    static void add()
+    {
+        System.out.println("some text added");
     }
 }
