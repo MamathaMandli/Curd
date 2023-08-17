63feaac48abe5e9858d1363c2bc10ab7f81da40d
mamatha.m <mmamatha0808@gmail.com>
2023-08-17 16:55:56 +0530

    this is method

--- src/Pune.java
+++ src/Pune.java
@@ -3,4 +3,8 @@ public class Pune
     public static void main(String[] args){
         System.out.println("demo");
     }
+    static void run()
+    {
+        System.out.println("this is method");
+    }
 }
